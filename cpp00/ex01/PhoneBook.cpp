2c078f6a9d538de05b5607254b44720921f40051
Salah-eddine Harrachmin <sharrach@e2r5p4.1337.ma>
2023-02-15 19:14:18 +0100

    cpp00

---
+++ cpp00/ex01/PhoneBook.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sharrach <sharrach@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 16:09:09 by sharrach          #+#    #+#             */
+/*   Updated: 2023/02/13 17:05:57 by sharrach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PhoneBook.hpp"
+
+int main()
+{
+	while(1)
+	{
+		char *message;
+		std::cout << "Welcome to the PhoneBook" << std::endl;
+		std::cin >> message;
+		std::cout << std::endl;
+	}
+	return 0;
+}
---
+++ cpp00/ex01/PhoneBook.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sharrach <sharrach@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 16:09:12 by sharrach          #+#    #+#             */
+/*   Updated: 2023/02/13 17:05:42 by sharrach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_H
+# define PHONEBOOK_H
+
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+
+#endif

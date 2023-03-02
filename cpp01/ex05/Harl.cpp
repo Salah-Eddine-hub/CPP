70cf970422d3e8711264226821e65c71fbacff4d
Salah-eddine Harrachmin <sharrach@e2r4p1.1337.ma>
2023-03-02 15:40:09 +0100

    define the messages

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -6,12 +6,28 @@
 /*   By: sharrach <sharrach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 17:32:40 by sharrach          #+#    #+#             */
-/*   Updated: 2023/03/02 10:26:39 by sharrach         ###   ########.fr       */
+/*   Updated: 2023/03/02 14:31:47 by sharrach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-void Harl::Harl(std::string level){
+void Harl::debug(){
+	std::cout << DEBUG << std::endl;
+}
+
+void Harl::info(){
+	std::cout << INFO << std::endl;
+}
+
+void Harl::warning(){
+	std::cout << WARNING << std::endl;
+}
+
+void Harl::error(){
+	std::cout << ERROR << std::endl;
+}
+
+void Harl::complain(std::string level){
 	
 }
